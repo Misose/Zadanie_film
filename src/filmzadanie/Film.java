406fa93aa76ddaccc5f4318a41c98af7d7dc0c05
Misose <miso216@azet.sk>
2015-06-19 09:04:23 +0200

    pokus o final verziu

--- src/filmzadanie/Film.java
+++ src/filmzadanie/Film.java
@@ -6,22 +6,111 @@ package filmzadanie;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 /**
  *
  * @author R7
  */
 public class Film {
-     private int ID_filmu;
+    //definovanie potrebnych parametrov (premennych)
+    //a ich funkcie, set, get
+
+    private int ID_filmu;
     private String Nazov_filmu;
-    private Date Cas;
-    
-    public static SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd HH:mm:ss ");
-    
     private int Dlzka_trvania;
     private String Jednoduchy_popis;
     private Date Datum_posledneho_pozicania;
     private Date Datum_posledneho_vratenia;
     private boolean Je_pozicany;
-    private int testing;
-    
+    // definovanie triedy datumu
+    private Date Cas;
+    // definovanie formatu casu
+    public static SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+
+    //constructor definovanie hodnot premennych
+    public Film(int ID_filmu, String Nazov_filmu, int Dlzka_trvania, String Jednoduchy_popis, /*Date Datum_posledneho_pozicania,*/ Date Datum_posledneho_vratenia, boolean Je_pozicany) //Funkcia nastavenia jednotlivých parametrov okrem posledného požičania - void
+    {
+        //nastavenie jednotlivych parametrov zo vstupu    
+        this.ID_filmu = ID_filmu;
+        this.Nazov_filmu = Nazov_filmu;
+        this.Dlzka_trvania = Dlzka_trvania;
+        this.Jednoduchy_popis = Jednoduchy_popis;
+        //this.Datum_posledneho_pozicania=Datum_posledneho_pozicania;
+        this.Datum_posledneho_vratenia = Datum_posledneho_vratenia;
+        this.Je_pozicany = Je_pozicany;
+
+    }
+
+    //  Metoda požičania – nastaví, dátum posledného požičania a hodnotu, či je požičaný – vráti true/false, podľa toho či je možné tento film požičať
+    public boolean Funkcia_pozicania(Date Datum_posledneho_pozicania) //Funkcia nastavenia jednotlivých parametrov okrem posledného požičania - void
+    {
+        //nastavenie jednotlivych parametrov zo vstupu    
+        this.Datum_posledneho_pozicania = Datum_posledneho_pozicania;
+        if (!this.Je_pozicany) //kontrola ci sa da pozicat
+        {
+            this.Je_pozicany = true;
+            return true;    //znamena ze sa da pozicat
+        } else {
+            return false; //znamena ze sa neda pozicat
+
+        }
+
+    }
+
+    public long Funkcia_vratenia(Date Datum_posledneho_vratenia) //Funkcia nastavenia jednotlivých parametrov okrem posledného požičania - void
+    {
+        long pocet_dni = 0;
+        final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+        //nastavenie jednotlivych parametrov zo vstupu    
+        this.Datum_posledneho_vratenia = Datum_posledneho_vratenia;
+
+        if (this.Je_pozicany) {
+            this.Je_pozicany = false;
+
+            if (!(this.Datum_posledneho_pozicania == null)) {
+                return (this.Datum_posledneho_vratenia.getTime() - this.Datum_posledneho_pozicania.getTime()) / DAY_IN_MILLIS; //definovanie datum_posledneho_vratenia-datum_posledneho_pozicania
+            } else {
+                return -1;
+            }
+
+        } else {
+            return -1;
+        }
+
+    }
+
+    public int get_Film_ID_filmu() {
+        return this.ID_filmu;
+
+    }
+
+    public String get_Film_Nazov_filmu() {
+        return this.Nazov_filmu;
+
+    }
+
+    public int get_Film_Dlzka_trvania() {
+        return this.Dlzka_trvania;
+
+    }
+
+    public String get_Film_Jednoduchy_popis() {
+        return this.Jednoduchy_popis;
+
+    }
+
+    public Date get_Film_Datum_posledneho_pozicania() {
+        return this.Datum_posledneho_pozicania;
+
+    }
+
+    public Date get_Film_Datum_posledneho_vratenia() {
+        return this.Datum_posledneho_vratenia;
+
+    }
+
+    public boolean get_Film_Je_pozicany() {
+        return this.Je_pozicany;
+
+    }
 }
--- src/filmzadanie/FilmZadanie.java
+++ src/filmzadanie/FilmZadanie.java
@@ -7,10 +7,13 @@ package filmzadanie;
 import java.io.*;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 /**
  *
  * @author R7
@@ -22,82 +25,77 @@ public class FilmZadanie {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        System.out.println("Current Date: " + Film.ft.format(new Date()));
-        
-        
-        
-        JSONObject obj = new JSONObject();
-	obj.put("name", "mkyong.com");
-	obj.put("age", new Integer(100));
- 
-	JSONArray list = new JSONArray();
-	list.add("msg 1");
-	list.add("msg 2");
-	list.add("msg 3");
- 
-	obj.put("messages", list);
- 
-	try {
- 
-		FileWriter file = new FileWriter("c:\\test.json");
-		file.write(obj.toJSONString());
-		file.flush();
-		file.close();
- 
-	} catch (IOException error) {
-		error.printStackTrace();
-	}
- 
-	System.out.print(obj);
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+        System.out.println("Aktualny cas: " + Film.ft.format(new Date()));
+
+
+
+        //citanie JSONU
         JSONParser parser = new JSONParser();
- 
-	try {
-		Object obja = parser.parse(new FileReader("c:\\test.json"));
- 
-		JSONObject jsonObject = (JSONObject) obja;
- 
-		String name = (String) jsonObject.get("name");
-		System.out.println(name);
- 
-		long age = (Long) jsonObject.get("age");
-		System.out.println(age);
- 
-		// loop array
-		JSONArray msg = (JSONArray) jsonObject.get("messages");
-		Iterator<String> iterator = msg.iterator();
-		while (iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
- 
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} catch (ParseException e) {
-		e.printStackTrace();
-	}
-        
-        
-        
-        
-        
-        
-        
- 
-        
-        
-        
-        
-    
+        try {
+            Object obj2 = parser.parse(new FileReader(args[0]));
+
+            JSONObject jsonObject1 = (JSONObject) obj2;
+
+            JSONArray slideContent = (JSONArray) jsonObject1.get("Filmy");
+
+            Film[] filmy = new Film[slideContent.size()]; // vytvorenie pola objektov podla poctu filmov
+
+            if (!(slideContent == null)) //ak neexistuje ziaden zaznam, teoreticky osetrit treba aj pre kazdu premennu.. ale predpokladm ze *.json je OK
+            {
+                Iterator i = slideContent.iterator();
+                int poradie_cyklu = 0;
+                while (i.hasNext()) {
+                    JSONObject slide = (JSONObject) i.next();
+                    //  String title = (String) slide.get("title");
+                    int ID_filmu = (int) (long) slide.get("ID_filmu");
+                    String Nazov_filmu = (String) slide.get("Nazov_filmu");
+                    int Dlzka_trvania = (int) (long) slide.get("Dlzka_trvania");
+                    String Jednoduchy_popis = (String) slide.get("Jednoduchy_popis");
+
+                    Date Datum_posledneho_pozicania = Film.ft.parse((String) slide.get("Datum_posledneho_pozicania"));
+
+                    Date Datum_posledneho_vratenia = null;
+
+                    Datum_posledneho_vratenia = Film.ft.parse((String) slide.get("Datum_posledneho_vratenia"));
+
+                    boolean Je_pozicany = (boolean) slide.get("Je_pozicany");
+
+                    filmy[poradie_cyklu] = new Film(ID_filmu, Nazov_filmu, Dlzka_trvania, Jednoduchy_popis, Datum_posledneho_vratenia, Je_pozicany);
+                    poradie_cyklu++;
+
+                }
+
+
+            }
+            //koniec citania JSONU
+            GuiFrame gui = new GuiFrame(filmy); // vytvorenie GUI
+            gui.setVisible(true); //zobrazenie gui
+
+        } catch (FileNotFoundException e) { //obidenie chyby programu
+            e.printStackTrace();
+            System.out.println("Nenaslo zadany subor, spusti program znova " + args[0]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+
+
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+            System.out.println("Problem s priradenim datumu z json");
+
+        }
+
+
+
+
+
+
     }
 }
